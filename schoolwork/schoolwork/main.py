bfeb5782b9dbcf5a21eb9f16b824ae2b31ea1979
rezi209 <qurezi899@gmail.com>
2024-11-04 22:17:40 +0400

    Add files via upload

---
+++ schoolwork/schoolwork/main.py
@@ -0,0 +1,39 @@
+print (10 % 5)
+print (10 % 3)
+print (20 % 6)
+print (30 % 7)
+print (40 % 8)
+print (50 % 6)
+print (17 % 5)
+print (18 % 4)
+print (23 % 4)
+
+
+int = 13 + 5
+int (2 ** 2)
+int (2 * 2 * 2 * 2)
+print (2 * 2 * 2)
+str = 1 
+ing = nam1
+str = 2
+ing = nam2
+str = 3
+int =nam3
+
+(nam3 + nam2 + nam1)
+(1 + 2 + 3)
+
+
+print (5 + 5)
+
+print (10 + 10)
+
+print (2 * 2)
+
+
+
+
+
+
+
+
